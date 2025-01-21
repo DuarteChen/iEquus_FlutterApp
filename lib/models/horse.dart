6dbfb995007f42bc7759592dc4926d1566ff6c86
Duarte Chen <duartechen@gmail.com>
2025-01-21 12:42:38 +0000

    adding data structures

---
+++ lib/models/horse.dart
@@ -0,0 +1,13 @@
+import 'package:equu';
+
+class Horse {
+  Horse({
+    required this.idClient,
+    required this.name,
+    required this.birthDate,
+  });
+
+  final int idClient;
+  final String name;
+  final DateTime birthDate;
+}
